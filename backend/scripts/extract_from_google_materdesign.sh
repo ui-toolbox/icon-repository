a80fd565ec31fa288068d1dae1f739d183a99c66
Kovács, Péter <peter.dunay.kovacs@gmail.com>
2018-07-04 12:26:58 +0200

    ISSUE-16: Import icons
    Script to create demo data from Google's Material Design icons
    - Filter further the icons based on the modulo of the icon name length
    - Print out the characteristics of test data created

--- backend/scripts/extract_from_google_materdesign.sh
+++ backend/scripts/extract_from_google_materdesign.sh
@@ -23,52 +23,69 @@
 
 path_to_google_matdes_icon_repo=${HOME}/github/google/material-design-icons
 destination_root=$(readlink -f $(dirname $0)/../demo-data)
-mkdir -p $destination_root
+selector_name_length_modulus=2
+
+
+rm -rf "$destination_root"
+mkdir $destination_root
 
 echo "destination_root: $destination_root"
 
 cd "$path_to_google_matdes_icon_repo"
 
+declare -A icon_count
+file_count=0
+
+filter_by_icon_name_length_mod() {
+    (( ${#icon_name} % $selector_name_length_modulus == 0 ))
+}
+
 store() {
     f=$1 size=$2; format=$3; icon_name=$4;
 
-set -x
+    filter_by_icon_name_length_mod || return
+
     destdir="$destination_root/$format/$size"
+set -x
     mkdir -p "$destdir"
     cp $f $destdir/$icon_name.$format
+    icon_count["$icon_name"]=$((icon_count["$icon_name"]+1))
 set +x
+    file_count=$((file_count+1))
 }
 
 raster() {
-    find . -type f | \
-        egrep '(^\./editor/.*$|^\./hardware/.*$)' | \
-        grep -v /ios/ | \
-        grep drawable-hdpi | \
-        egrep '*_white_[^.]+.[a-z]+' | \
-        sort | \
-        while read f;
-        do
-            icon_name=$(echo "$f" | sed -e 's/.*\/ic_\([^.]\+\)_white_[^.]\+\.[^.]\+/\1/g')
-            format=$(echo "$f" | sed -e 's/.*\/ic_[^.]\+_white_[^.]\+\.\([^.]\+\)/\1/g')
-            size=$(echo "$f" | sed -e 's/.*\/ic_[^.]\+_white_\([^.]\+\)\.[^.]\+/\1/g')
-            store $f $size $format $icon_name
-        done
+    while read f;
+    do
+        icon_name=$(echo "$f" | sed -e 's/.*\/ic_\([^.]\+\)_white_[^.]\+\.[^.]\+/\1/g')
+        format=$(echo "$f" | sed -e 's/.*\/ic_[^.]\+_white_[^.]\+\.\([^.]\+\)/\1/g')
+        size=$(echo "$f" | sed -e 's/.*\/ic_[^.]\+_white_\([^.]\+\)\.[^.]\+/\1/g')
+        store $f $size $format $icon_name
+    done < <(find . -type f | \
+                egrep '(^\./editor/.*$|^\./hardware/.*$)' | \
+                grep -v /ios/ | \
+                grep drawable-hdpi | \
+                egrep '*_white_[^.]+.[a-z]+' | \
+                sort)
 }
 
 svg() {
-    find . -type f | \
-        egrep '(^\./editor/.*$|^\./hardware/.*$)' | \
-        grep -v /ios/ | \
-        grep svg | \
-        sort | \
-        while read f;
-        do
-            icon_name=$(echo "$f" | sed -e 's/.*\/ic_\([^.]\+\)_[^._]\+\.svg/\1/g')
-            format=svg
-            size=$(echo "$f" | sed -e 's/.*\/ic_[^.]\+_\([^._]\+\)\.svg/\1/g')
-            store $f $size $format $icon_name
-        done
+    while read f;
+    do
+        icon_name=$(echo "$f" | sed -e 's/.*\/ic_\([^.]\+\)_[^._]\+\.svg/\1/g')
+        format=svg
+        size=$(echo "$f" | sed -e 's/.*\/ic_[^.]\+_\([^._]\+\)\.svg/\1/g')
+        store $f $size $format $icon_name
+    done < <(find . -type f | \
+                egrep '(^\./editor/.*$|^\./hardware/.*$)' | \
+                grep -v /ios/ | \
+                grep svg | \
+                sort)
 }
 
 raster
 svg
+
+cd "$destination_root"
+du -h .
+echo "Stored ${#icon_count[@]} icon(s) in $file_count file(s)"
