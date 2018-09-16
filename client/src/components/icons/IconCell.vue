554dfc0a4d95610a41bf19b891ebb10a130ef7af
Kovács, Péter <peter.dunay.kovacs@gmail.com>
2018-09-16 22:27:40 +0200

    View- and edit-control in icon-cell to display/edit icon details
    The responsibility of triggering view and edit is transferred
    from simple icon-cell selection to dedicated controls in
    anticipation of selecting multiple icons to download/icon-font
    generation.

--- client/src/components/icons/IconCell.vue
+++ client/src/components/icons/IconCell.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="icon-cell animated fadeIn zoomIn">
-    <div class="icon-preview">
-      <img v-bind:src="firstPath" height="30" @click="iconSelected">
+    <div class="icon-cell">
+        <div v-if="!editable" class="el-icon-view iconcell-control" @click="viewRequested"/>
+        <div v-if="editable" class="el-icon-edit iconcell-control" @click="editRequested"/>
+        <div class="icon-preview">
+        <img v-bind:src="firstPath" height="30">
+        </div>
+        <div class="icon-name">{{icon.name}}</div>
     </div>
-    <div class="icon-name">{{icon.name}}</div>
-  </div>
 </template>
 
 <script>
 import getEndpointUrl from '@/services/url';
 export default {
     name: 'IconCell',
-    props: ['icon'],
+    props: ['icon', 'editable'],
     computed: {
         firstPath() {
             // If the icon has SVG format, prefer that
@@ -20,8 +22,11 @@ export default {
         }
     },
     methods: {
-        iconSelected() {
-            this.$emit('iconSelected', this.icon);
+        viewRequested() {
+            this.$emit('view', this.icon);
+        },
+        editRequested() {
+            this.$emit('edit', this.icon);
         }
     }
 }
@@ -42,24 +47,37 @@ $icon-preview-size: 50px;
     // background-color: #CCC;
     margin: 0 auto;
     text-align: center;
-    margin-top: 20px;
+    margin-top: 40px;
   }
   .icon-name {
     position: absolute;
     font-family: "Roboto-Light";
-    bottom: 10px;
+    bottom: 30px;
     width: calc(100% - 2*10px);
     text-align: center;
     font-size: 13px;
     overflow: hidden;
     white-space: nowrap;
-  text-overflow: ellipsis;
+    text-overflow: ellipsis;
+  }
+  div.iconcell-control {
+        position: absolute;
+        top: 6px;
+        right: 9px;
+        opacity: 0;
+        transition: opacity 0.3s 0.1s cubic-bezier(0.4, 0.7, 0.99, 0.99), transform 0.2s ease-in-out;
+        &:hover {
+            transform: scale(2.0);
+        }
   }
   img {
       transition: all 0.3s ease-in-out;
   }
   &:hover {
     background-color: #F0F0F0;
+    div.iconcell-control {
+        opacity: 1;
+    }
     img {
       transform: scale(2.0);
     }
